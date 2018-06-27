4c7515793ae6fe4f8608eea98380834af7c6f9a8
Daniel Stone <ds.danstone@gmail.com>
2018-06-27 11:58:48 -0400

    Added test classes and tested ParkDAL

---
+++ Capstone.Tests/SiteDALTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Transactions;
+using Capstone.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Tests
+{
+	[TestClass]
+	public class SiteDALTests :CampingTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
--- Capstone/DAL/CampgroundDAL.cs
+++ Capstone/DAL/CampgroundDAL.cs
@@ -9,5 +9,7 @@ namespace Capstone.DAL
 {
     class CampgroundDAL
     {
+
+
     }
 }
