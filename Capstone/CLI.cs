1a2a0bfcb60202a4bdd78ebe60df5eef64c39c5e
Bakhary Drummond <bdrummond3625@bths.edu>
2018-06-27 14:37:10 -0400

    1436 fixing Menu Structure

--- Capstone/CLI.cs
+++ Capstone/CLI.cs
@@ -22,11 +22,13 @@ namespace Capstone
         /// <summary>
         /// Initializes the program interface
         /// </summary>
-        public void Run()
+        public void Run()       //  SHOULD CALL A MAIN MENU "WRAPPER METHOD"
         {
             PrintHeader();
             // Print the list of Parks
             CurrentPark = PrintMainMenu();
+            GetParkInfo(CurrentPark);
+
             if (CurrentPark == null)
             {
                 return;
@@ -53,6 +55,25 @@ namespace Capstone
 
         }
 
+        public int GetParkInfo(Park park)
+        {
+            Console.Clear();
+            Console.WriteLine(park.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1) View Campgrounds");    // View Campgrounds in this park
+            Console.WriteLine("2) Search for Reservation");    //2) Search for Reservation in this park
+            Console.WriteLine("3) Return to Previous Screen");    // 3) Return to Previous Screen
+
+
+
+            int userChoice = GetInteger(Console.ReadLine());
+
+
+            return userChoice;
+
+
+        }
         /// <summary>
         /// Displays a list of campgrounds at the specified park with options to select a site, search for availability, or quit.
         /// </summary>
@@ -90,47 +111,66 @@ namespace Capstone
         /// </summary>
         public Park PrintMainMenu()      // Make this return the chosen park
         {
-            Console.WriteLine("Available Parks");
-            Console.WriteLine();
-            //Call ParkDAL to get all the parks
-            //Get a list of park objects from ParkDAL
-            IList<Park> parks = parkDAL.GetParks();
-
-            //Choice number displayed
-            int menuOption = 1;
-            foreach (var park in parks)
+            bool viewMenu = true;
+            while (viewMenu)
             {
-                Console.WriteLine($"{menuOption}) {park.Name}");
-                menuOption++;
-            }   //Display all of the parks
+                Console.WriteLine("Available Parks");
+                Console.WriteLine();
+                //Call ParkDAL to get all the parks
+                //Get a list of park objects from ParkDAL
+                IList<Park> parks = parkDAL.GetParks();
 
-            Console.WriteLine("Q) Quit");
-            Console.WriteLine();
+                //Choice number displayed
+                int menuOption = 1;
+                foreach (var park in parks)
+                {
+                    Console.WriteLine($"{menuOption}) {park.Name}");
+                    menuOption++;
+                }   //Display all of the parks
 
-            string userChoice = Console.ReadLine().ToUpper();       //User selects a park to get more info on
-            if (userChoice == "Q")                    //Quit option
-            {
-                return null;
+                Console.WriteLine("Q) Quit");
+                Console.WriteLine();
+
+                string userChoice = Console.ReadLine().ToUpper();       //User selects a park to get more info on
+                if (userChoice == "Q")                    //Quit option
+                {
+                    return null;
+                }
+                var choiceNum = GetInteger(userChoice);     // If not quit, it needs to be a number
+                if (choiceNum > parks.Count - 1)
+                {
+                    while (choiceNum > parks.Count - 1)
+                    {
+                        Console.WriteLine("Try Again.");
+                        choiceNum = GetInteger(Console.ReadLine());
+                    }
+
+                }              // Within the range of possible parks
+                if (choiceNum <= 0)
+                {
+                    while (choiceNum < 0)
+                    {
+                        Console.WriteLine("Try Again.");
+                        choiceNum = GetInteger(Console.ReadLine());
+                    }
+                }                               //  ""
+                Park infoPark = parks[choiceNum];
+               int infoChoice= GetParkInfo(infoPark);              // Prints Park info
+
+                switch (infoChoice)
+                {
+                    case 1:
+
+
+
+
+                    default:
+                        break;
+                }
+
+
+
             }
-            var choiceNum = GetInteger(userChoice);     // If not quit, it needs to be a number
-            if (choiceNum > parks.Count - 1)
-            {
-                while (choiceNum > parks.Count - 1)
-                {
-                    Console.WriteLine("Try Again.");
-                    choiceNum = GetInteger(Console.ReadLine());
-                }
-
-            }              // Within the range of possible parks
-            if (choiceNum <= 0)
-            {
-                while (choiceNum < 0)
-                {
-                    Console.WriteLine("Try Again.");
-                    choiceNum = GetInteger(Console.ReadLine());
-                }
-            }                               //  ""
-            Park infoPark = parks[choiceNum];
             return infoPark;
         }
 
