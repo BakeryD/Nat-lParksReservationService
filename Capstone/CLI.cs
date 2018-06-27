a4cfe9f46b06373bc2649c742988fce912c9e896
Bakhary Drummond <bdrummond3625@bths.edu>
2018-06-27 12:14:28 -0400

    PrintMainMenu complete 1214

--- Capstone/CLI.cs
+++ Capstone/CLI.cs
@@ -1,4 +1,5 @@
-﻿using Capstone.Models;
+﻿using Capstone.DAL;
+using Capstone.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace Capstone
         private const string ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Campground;Integrated Security=True";
         private Park CurrentPark;
         private Campground CurrentGround;
-
+        private static ParkDAL parkDAL = new ParkDAL(ConnectionString); 
 
 
 
@@ -24,16 +25,17 @@ namespace Capstone
         {
             PrintHeader();
             // Print the list of Parks
-            PrintMainMenu();
-            // Accept User input
-            string userChoice = Console.ReadLine().ToUpper();       //User selects a park to get more info on
-
-            // Call print park menu based on user input
-            PrintParkMenu(userChoice);                              //
-            userChoice = Console.ReadLine().ToUpper();
+           CurrentPark = PrintMainMenu();
+            if (CurrentPark==null)
+            {
+                return;
+            } // If they quit, current park stays null and this ends the program
+           // Call print park menu based on user input
+            PrintParkMenu(CurrentPark);                              // User gets park info and choice to view campgrounds or search for reservation
+           //   userChoice = Console.ReadLine().ToUpper();
 
             // call print campground menu based on user input
-            PrintCampGroundMenu(userChoice);
+            //  PrintCampGroundMenu(userChoice);
 
 
         }
@@ -51,23 +53,29 @@ namespace Capstone
             
         }
 
-        public void PrintParkMenu(string choice)
+        public void PrintParkMenu(Park chosenPark)
         {
+            //CurrentPark.ToString();
 
+
+
+            //User can view campgrounds in park or search for availability in a specified date range
           
 
 
 
         }
         /// <summary>
-        /// Displays a list of available parks with the option to get more info.
+        /// Displays a list of available parks with the option to get more info and returns the result
         /// </summary>
-        public void PrintMainMenu()
+        public Park PrintMainMenu()      // Make this return the chosen park
         {
+            Console.WriteLine("Available Parks");
+            Console.WriteLine();
                  // SQL time!
             //Call ParkDAL to get all the parks
             //Get a list of park objects from ParkDAL
-            List<Park> parks=new List<Park>();
+            IList<Park> parks= parkDAL.GetParks();
 
             //Choice number displayed
             int menuOption = 1;
@@ -77,9 +85,69 @@ namespace Capstone
                 menuOption++;
             }
 
+            Console.WriteLine("Q) Quit");
             Console.WriteLine();
-            Console.WriteLine();
+
+            string userChoice = Console.ReadLine().ToUpper();       //User selects a park to get more info on
+            if (userChoice=="Q")                    //Quit option
+            {
+                return null;
+            }
+
+            var choiceNum = GetInteger(userChoice);     // If not quit, it needs to be a number
+
+             if (choiceNum > parks.Count-1)
+             {
+                while (choiceNum>parks.Count-1)
+                {
+                    Console.WriteLine("Try Again.");
+                    choiceNum = GetInteger(Console.ReadLine());
+                }
+
+             }              // Within the range of possible parks
+            if (choiceNum<0)
+            {
+                while (choiceNum < 0)
+                {
+                    Console.WriteLine("Try Again.");
+                    choiceNum = GetInteger(Console.ReadLine());
+                }
+            }                               //  ""
+
+
+            Park infoPark = parks[choiceNum];       
+
+
+            return infoPark;
         }
+
+        public static int GetInteger(string message)
+        {
+            string userInput = message;
+            int intValue = 0;
+            int numberOfAttempts = 0;
+
+            do
+            {
+                if (numberOfAttempts > 0)
+                {
+                    Console.WriteLine("Invalid Menu Choice, Try Again.");
+                }
+
+                if (int.TryParse(message, out intValue))
+                {
+                    return intValue;
+                }
+                //Console.Write(message + " ");
+                userInput = Console.ReadLine();
+                numberOfAttempts++;
+            }
+            while (!int.TryParse(userInput, out intValue));
+
+            return intValue;
+
+        }
+
 
 
     }
