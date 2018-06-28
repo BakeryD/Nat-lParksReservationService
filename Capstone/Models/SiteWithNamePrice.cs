6d055573b67a460077f4e108897e21c551bace4a
Daniel Stone <ds.danstone@gmail.com>
2018-06-27 22:39:02 -0400

    created SiteWithNamePrice class and modified findavailablesites to use it

---
+++ Capstone/Models/SiteWithNamePrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+	public class SiteWithNamePrice : Site
+	{
+		public string Name { get; set; }
+		public decimal DailyFee { get; set; }
+
+		public override string ToString()
+		{
+			return this.Name.PadRight(15) + base.ToString();
+		}
+	}
+}
